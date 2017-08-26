7b29ec649868faf1b7c7b07e5d8244240b451067
CarlosVictoria <carlosvictoriarangel@gmail.com>
2017-08-26 12:32:44 -0500

    se agrego los services de roles y tipoDocumento

--- client/app/app.js
+++ client/app/app.js
@@ -1,25 +1,25 @@
 'use strict';
 
 angular.module('eventosApp', [
-  'eventosApp.constants',
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ui.router',
-  'ui.bootstrap',
-  'satellizer'
-])
-  .constant("API", "")
+        'eventosApp.constants',
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ui.router',
+        'ui.bootstrap',
+        'satellizer'
+    ])
+    .constant("API", "")
 
-  //config satellizer
+//config satellizer
 .config(function(API, $authProvider) {
-    $authProvider.loginUrl = API + '/api/auth/login';
-    $authProvider.tokenName = 'token';
-    $authProvider.tokenPrefix = 'videoClub';
-})
-  .config(function($urlRouterProvider, $locationProvider) {
-    $urlRouterProvider
-      .otherwise('/');
+        $authProvider.loginUrl = API + '/api/auth/login';
+        $authProvider.tokenName = 'token';
+        $authProvider.tokenPrefix = 'eventos';
+    })
+    .config(function($urlRouterProvider, $locationProvider) {
+        $urlRouterProvider
+            .otherwise('/');
 
         $locationProvider.html5Mode(true);
     });
