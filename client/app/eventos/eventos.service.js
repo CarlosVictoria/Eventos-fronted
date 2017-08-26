f98865771fffad6b7e739a358a5f0c4b46189768
CarlosVictoria <carlosvictoriarangel@gmail.com>
2017-08-26 12:03:52 -0500

    se soluciono problema en eventos-list

--- client/app/eventos/eventos.service.js
+++ client/app/eventos/eventos.service.js
@@ -1,6 +1,14 @@
 'use strict';
 
+function eventosService($resource, API) {
+    return $resource(API + '/api/usuarios/:id', {
+        id: '@id'
+    }, {
+        update: {
+            method: 'PUT'
+        }
+    })
+}
+eventosService.$inject = ['$resource', 'API'];
 angular.module('eventosApp')
-  .service('eventos', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+  .factory('eventosService', eventosService);
